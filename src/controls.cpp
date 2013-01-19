f3fb956acbf650cfe3343d784475b2315f6f9b70
lejar <christopher.pezley@rwth-aachen.de>
2013-01-19 23:36:50 +0100

    Update src/controls.cpp
    Increased viewing distance

--- src/controls.cpp
+++ src/controls.cpp
@@ -143,8 +143,8 @@ void computeMatricesFromInputs(){
 	//if(!GUIactive) 
 	//	FoV -= 5*glfwGetMouseWheel();
 
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Projection matrix : 45ï¿½ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 300.0f);
 	// Camera matrix
 	ViewMatrix       = glm::lookAt(
 								position,           // Camera is here
